cf47050affa3156be42de28f305ebcf2b7eec8bf
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-06-11 22:58:05 +0600

    making anagrams updated

--- Interview-Preparation-Kit/StringManipulation/makingAnagrams.js
+++ Interview-Preparation-Kit/StringManipulation/makingAnagrams.js
@@ -1,32 +1,19 @@
 const makeAnagram = (a, b) => {
-    const strHash1 = {};
-    const strHash2 = {};
-    const keys = [];
+    const strHash = {};
     let deleteCount = 0;
 
     a.split('').forEach(ch => {
-        strHash1[ch] = !strHash1[ch] ? 1 : strHash1[ch] + 1;
-        if(keys.indexOf(ch) === -1){
-            keys.push(ch)
-        } 
+        strHash[ch] = !strHash[ch] ? 1 : strHash[ch] + 1;
+        deleteCount++;
     });
     b.split('').forEach(ch => {
-        strHash2[ch] = !strHash2[ch] ? 1 : strHash2[ch] + 1
-        if(keys.indexOf(ch) === -1){
-            keys.push(ch)
-        } 
-    });
-
-    keys.forEach(ch => {
-        if(!strHash1[ch]){
-            deleteCount += strHash2[ch];
-        }else if(!strHash2[ch]){
-            deleteCount += strHash1[ch];
+        if(strHash[ch]){
+            strHash[ch]--;
+            deleteCount--;
         }else{
-            deleteCount += Math.abs(strHash1[ch] - strHash2[ch])
+            deleteCount++;
         }
-    })
-
+    });
 
     return deleteCount;
 }
