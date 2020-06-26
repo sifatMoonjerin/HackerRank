b4e9dfe0944630c0ac15aeacbdbfa4ee8b50cd78
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-06-26 09:31:30 +0600

    list comprehension

---
+++ python/listComprehensions.py
@@ -0,0 +1,13 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+result = []
+
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if (i+j+k) != n:
+                result.append([i,j,k])
+
+print(result)
