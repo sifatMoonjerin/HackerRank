980b830621b460b123483b35df547528ca07d2cb
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-07-24 21:57:27 +0600

    format

--- 30-Days-of-Code/day12.py
+++ 30-Days-of-Code/day12.py
@@ -14,15 +14,15 @@ class Student(Person):
         
     def calculate(self):
         average = sum(self.scores)/len(self.scores)
-        if average>=90:
+        if average >= 90:
             return 'O'
-        elif average>=80:
+        elif average >= 80:
             return 'E'
-        elif average>=70:
+        elif average >= 70:
             return 'A'
-        elif average>=55:
+        elif average >= 55:
             return 'P'
-        elif average>=40:
+        elif average >= 40:
             return 'D'
         else:
             return 'T'
